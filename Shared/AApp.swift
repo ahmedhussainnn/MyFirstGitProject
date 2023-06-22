96efb57fd8104b313ed2ec288a8116a66a40fbb2
Ahmed Usman Hussain <ahmedandoma@gmail.com>
2023-06-22 04:27:50 +0200

    Initial Commit

---
+++ Shared/AApp.swift
@@ -0,0 +1,17 @@
+//
+//  AApp.swift
+//  Shared
+//
+//  Created by mac on 22.06.23.
+//
+
+import SwiftUI
+
+@main
+struct AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
